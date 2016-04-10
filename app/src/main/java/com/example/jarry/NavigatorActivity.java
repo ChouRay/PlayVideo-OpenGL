28e81f75f806aa0796f9dc823ac2c2a3e4b75932
ChouRay <zhouzlove@live.com>
2016-04-10 17:22:55 +0800

    提交GLSurfaceViewMedia 相应代码

---
+++ app/src/main/java/com/example/jarry/NavigatorActivity.java
@@ -0,0 +1,45 @@
+package com.example.jarry;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.jarry.playvideo_glsurfaceview.GLViewMediaActivity;
+import com.example.jarry.playvideo_texuture.R;
+import com.example.jarry.playvideo_texuture.TextureViewMediaActivity;
+
+public class NavigatorActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Intent mIntent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigator);
+        mIntent = new Intent();
+        findViewById(R.id.button_texture_view).setOnClickListener(this);
+        findViewById(R.id.button_glsurfaceview).setOnClickListener(this);
+        findViewById(R.id.button_Surface_view_native).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_texture_view:
+                mIntent.setClass(this, TextureViewMediaActivity.class);
+                startActivity(mIntent);
+                Toast.makeText(this, "Play Video on TextureView", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_glsurfaceview:
+                mIntent.setClass(this, GLViewMediaActivity.class);
+                startActivity(mIntent);
+                Toast.makeText(this, "Play Video on GLSurfaceView", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_Surface_view_native:
+                Toast.makeText(this, "以后有空再整^^", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jarry/playvideo_glsurfaceview/GLViewMediaActivity.java
@@ -0,0 +1,242 @@
+package com.example.jarry.playvideo_glsurfaceview;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+
+import com.example.jarry.playvideo_texuture.R;
+import com.example.jarry.utils.RawResourceReader;
+import com.example.jarry.utils.ShaderHelper;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class GLViewMediaActivity extends AppCompatActivity implements GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener {
+
+    public static final String videoPath = Environment.getExternalStorageDirectory().getPath()+"/one.mp4";
+
+    private boolean frameAvailable = false;
+    int textureParamHandle;
+    int textureCoordinateHandle;
+    int positionHandle;
+    int textureTranformHandle;
+
+    /**
+     *
+     */
+    private static float squareSize = 1.0f;
+    private static float squareCoords[] = {
+            -squareSize, squareSize,   // top left
+            -squareSize, -squareSize,   // bottom left
+            squareSize, -squareSize,    // bottom right
+            squareSize, squareSize}; // top right
+
+    private static short drawOrder[] = {0, 1, 2, 0, 2, 3};
+
+    private Context context;
+
+    // Texture to be shown in backgrund
+    private FloatBuffer textureBuffer;
+    private float textureCoords[] = {
+            0.0f, 1.0f, 0.0f, 1.0f,
+            0.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 1.0f, 0.0f, 1.0f};
+    private int[] textures = new int[1];
+
+    private int width, height;
+
+    private int shaderProgram;
+    private FloatBuffer vertexBuffer;
+    private ShortBuffer drawListBuffer;
+    private float[] videoTextureTransform = new float[16];
+    private SurfaceTexture videoTexture;
+    private GLSurfaceView glView;
+    private MediaPlayer mediaPlayer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        context = this;
+        glView = new GLSurfaceView(this);
+        setContentView(glView);
+        glView.setEGLContextClientVersion(2);
+        glView.setRenderer(this);
+    }
+
+    private void playVideo() {
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+            Surface surface = new Surface(videoTexture);
+            mediaPlayer.setSurface(surface);
+            surface.release();
+            try {
+                mediaPlayer.setDataSource(videoPath);
+                mediaPlayer.prepareAsync();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        setupGraphics();
+        setupVertexBuffer();
+        setupTexture();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        this.width = width;
+        this.height = height;
+        playVideo();
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        synchronized (this) {
+            if (frameAvailable) {
+                videoTexture.updateTexImage();
+                videoTexture.getTransformMatrix(videoTextureTransform);
+                frameAvailable = false;
+            }
+        }
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        GLES20.glViewport(0, 0, width, height);
+        this.drawTexture();
+
+    }
+
+    @Override
+    public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+        synchronized (this) {
+            frameAvailable = true;
+        }
+    }
+
+    private void setupGraphics() {
+        final String vertexShader = RawResourceReader.readTextFileFromRawResource(context, R.raw.vetext_sharder);
+        final String fragmentShader = RawResourceReader.readTextFileFromRawResource(context, R.raw.fragment_sharder);
+
+        final int vertexShaderHandle = ShaderHelper.compileShader(GLES20.GL_VERTEX_SHADER, vertexShader);
+        final int fragmentShaderHandle = ShaderHelper.compileShader(GLES20.GL_FRAGMENT_SHADER, fragmentShader);
+        shaderProgram = ShaderHelper.createAndLinkProgram(vertexShaderHandle, fragmentShaderHandle,
+                new String[]{"texture", "vPosition", "vTexCoordinate", "textureTransform"});
+
+        GLES20.glUseProgram(shaderProgram);
+        textureParamHandle = GLES20.glGetUniformLocation(shaderProgram, "texture");
+        textureCoordinateHandle = GLES20.glGetAttribLocation(shaderProgram, "vTexCoordinate");
+        positionHandle = GLES20.glGetAttribLocation(shaderProgram, "vPosition");
+        textureTranformHandle = GLES20.glGetUniformLocation(shaderProgram, "textureTransform");
+    }
+
+    private void setupVertexBuffer() {
+        // Draw list buffer
+        ByteBuffer dlb = ByteBuffer.allocateDirect(drawOrder.length * 2);
+        dlb.order(ByteOrder.nativeOrder());
+        drawListBuffer = dlb.asShortBuffer();
+        drawListBuffer.put(drawOrder);
+        drawListBuffer.position(0);
+
+        // Initialize the texture holder
+        ByteBuffer bb = ByteBuffer.allocateDirect(squareCoords.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(squareCoords);
+        vertexBuffer.position(0);
+    }
+
+    private void setupTexture() {
+        ByteBuffer texturebb = ByteBuffer.allocateDirect(textureCoords.length * 4);
+        texturebb.order(ByteOrder.nativeOrder());
+
+        textureBuffer = texturebb.asFloatBuffer();
+        textureBuffer.put(textureCoords);
+        textureBuffer.position(0);
+
+        // Generate the actual texture
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glGenTextures(1, textures, 0);
+        checkGlError("Texture generate");
+
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textures[0]);
+        checkGlError("Texture bind");
+
+        videoTexture = new SurfaceTexture(textures[0]);
+        videoTexture.setOnFrameAvailableListener(this);
+    }
+
+    private void drawTexture() {
+        // Draw texture
+
+        GLES20.glEnableVertexAttribArray(positionHandle);
+        GLES20.glVertexAttribPointer(positionHandle, 2, GLES20.GL_FLOAT, false, 0, vertexBuffer);
+
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textures[0]);
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glUniform1i(textureParamHandle, 0);
+
+        GLES20.glEnableVertexAttribArray(textureCoordinateHandle);
+        GLES20.glVertexAttribPointer(textureCoordinateHandle, 4, GLES20.GL_FLOAT, false, 0, textureBuffer);
+
+        GLES20.glUniformMatrix4fv(textureTranformHandle, 1, false, videoTextureTransform, 0);
+
+        GLES20.glDrawElements(GLES20.GL_TRIANGLE_STRIP, drawOrder.length, GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+        GLES20.glDisableVertexAttribArray(positionHandle);
+        GLES20.glDisableVertexAttribArray(textureCoordinateHandle);
+    }
+
+    public void checkGlError(String op) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e("SurfaceTest", op + ": glError " + GLUtils.getEGLErrorString(error));
+        }
+    }
+
+}
